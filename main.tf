bcb611d2a44f516187cc5b1b3bb7c888be3ede1f
Suhrud Valaparla <suhrud.valaparla@peopletech.com>
2026-04-02 18:21:13 +0530

    jenkins

--- main.tf
+++ main.tf
@@ -1,7 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.99.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
 provider "azurerm" {
   features {}
 }
-
 # -----------------------------
 # Network (VNet + Subnets)
 # -----------------------------
